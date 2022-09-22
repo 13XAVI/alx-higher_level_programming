f51770e4a51d08026b9894efda6f7a65b6507a97
13XAVI <tresorxavier16@gmail.com>
2022-09-22 20:43:20 +0200

    my commit message

--- 0x02-python-import_modules/calculator_1.py
+++ 0x02-python-import_modules/calculator_1.py
@@ -1,19 +1,44 @@
 #!/usr/bin/python3
+
 def add(a, b):
-    """ the calculator
-    args:
-        a = first integer
-        b = second integer"""
+    """My addition function
+    Args:
+        a: first integer
+        b: second integer
+    Returns:
+        The return value. a + b
+    """
     return (a + b)
 
 
-def substract(a, b):
+def sub(a, b):
+    """My subtraction function
+    Args:
+        a: first integer
+        b: second integer
+    Returns:
+        The return value. a - b
+    """
     return (a - b)
 
 
-def divide(a, b):
-    return (a / b)
-
-
-def multiply(a, b):
+def mul(a, b):
+    """My multiplication function
+    Args:
+        a: first integer
+        b: second integer
+    Returns:
+        The return value. a * b
+    """
     return (a * b)
+
+
+def div(a, b):
+    """My division function
+    Args:
+        a: first integer
+        b: second integer
+    Returns:
+        The return value. a / b
+    """
+    return int(a / b)
