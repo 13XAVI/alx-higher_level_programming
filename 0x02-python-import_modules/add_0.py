05028282cadc50d91746368b61149cca4de1be7d
13XAVI <tresorxavier16@gmail.com>
2022-09-22 17:06:34 +0200

    my message

--- 0x02-python-import_modules/add_0.py
+++ 0x02-python-import_modules/add_0.py
@@ -1,3 +1,11 @@
 #!/usr/bin/python3
+
 def add(a,b):
-    return a+b
+    """ my addition function
+    args:
+        a: first integer
+        b: secon integer
+    Returns:
+        The return value. a+b
+    """
+    return(a+b)
