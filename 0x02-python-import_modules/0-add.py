460ffbe7c354c9782670bf645cce4ca9fe63c1b4
13XAVI <tresorxavier16@gmail.com>
2022-09-22 17:31:16 +0200

    my message

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-import add_0
+
 if __name__ == "__main__":
     """Print the sum of 1 and2. """
+import add_0
 a = 1
 b = 2
 print('{} + {} ='.format(1, 2), add_0.add(a, b))
-print(end="\n")
