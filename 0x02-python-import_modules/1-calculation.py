39b27e35777df9cec547788db21bfffddb3ef197
13XAVI <tresorxavier16@gmail.com>
2022-09-22 18:23:51 +0200

    my message

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,5 +1,12 @@
 #!/usr/bin/python3
-from calculator_1.py import*
+if __name__ == "__main__":
+    """print sum,multiply,difference,divide"""
+from calculator_1 import add, substract, divide, multiply
 a = 10
 b = 5
-print (add(10,5))
+
+
+print("{}+{} = ".format(a, b), add(10, 5))
+print("{}-{} = ".format(a, b), substract(10, 5))
+print("{}*{} = ".format(a, b), multiply(10, 5))
+print("{}/{} = ".format(a, b), divide(10, 5))
--- 0x02-python-import_modules/calculator_1.py
+++ 0x02-python-import_modules/calculator_1.py
@@ -1,11 +1,19 @@
 #!/usr/bin/python3
-def add(a,b):
-    return a+b
-def substract(a,b):
-    return a-b
-def divide(a,b):
-    return a*b
-def multiply(a,b):
-    return a/b
+def add(a, b):
+    """ the calculator
+    args:
+        a = first integer
+        b = second integer"""
+    return (a + b)
 
 
+def substract(a, b):
+    return (a - b)
+
+
+def divide(a, b):
+    return (a * b)
+
+
+def multiply(a, b):
+    return (a / b)
