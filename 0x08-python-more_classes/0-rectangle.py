7af337a21ad76b8af633938b9e6501ef2554f3f9
13XAVI <tresorxavier16@gmail.com>
2022-11-05 10:34:51 +0200

    Add an empty class

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,3 +1,7 @@
 #usr/bin/python3
 class Rectangle:
     """this is an Empty rectangle class"""
+    def __init__(lenght=0,width=0):
+        """my class attributes"""
+        self.__lenght = lenght
+        self.__width = width
